6097ec0c8973a946d86a0d6a4b439b1c559f5306
Thijs van <80729860+MathijsvanDriel@users.noreply.github.com>
2021-06-16 06:59:51 +0000

    FraserCanteen_v1

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Thijs van Driel This sequence 
+
 import time
 print("Welcome to the Fraser High School canteen") 
 time.sleep(1.5) 
@@ -9,8 +11,19 @@ print("We sell pies and burgers at the Fraser High canteen!")
 buy = input ("Would you like a pie, or a burger, " + n + "?"'\n')
 time.sleep(1.5) 
 
-if buy.lower() == 'burger':
-  time.sleep(1.5)
+if buy.lower() == 'burger': 
   print("Thats great") 
-  time.sleep(1.5)
+  time.sleep(1.5) 
+  print("The burger will be he ready in 5 seconds") 
+  time.sleep(5)  
+  print("🍔") 
+
+if buy.lower() == 'pie':
+  print("Pie are my favourite") 
+  time.sleep(1.5) 
+  print("The pie will be he ready in 5 seconds") 
+  time.sleep(5) 
+  print("Here is a fresh hot mince pie") 
+  print("🥧")
+ 
 
